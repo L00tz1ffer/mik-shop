0ee4ddb057ab2c5bcdef7dec589e3296f81163b0
L00tz1ffer <unknown@host.docker.internal>
2021-08-17 22:28:12 +0200

    Revert "Ich habe keine Ahnung wo bei mir der Fehler liegt aber es werden zu keinem Zeitpunkt cookies gespeichert... kann mir bitte jemand helfen?"
    This reverts commit b2df12d22a0e5a39af28be8e244f45e5e685db69.

---
+++ shop/includes.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once __DIR__.'/function/database.php';
+require_once __DIR__.'/config/vars.php';
+require_once __DIR__.'/function/user.php';
+require_once __DIR__.'/function/cart.php';
+require_once __DIR__.'/function/product.php';
+require_once __DIR__.'/routes.php';
---
+++ shop/templates/default/card.php
@@ -0,0 +1,16 @@
+<div class="card">
+	<div class="card-title">
+		<?= $product['title']?> 
+	</div>
+	<img src="http://placekitten.com/200/170" class="card-img-top" alt="Produkt">
+	<div class="card-body">
+		<?= $product['description'] ?>
+		<hr>
+
+                <?= number_format($product['price']/100,2,","," ") ?> €
+	</div>
+	<div class="card-footer">
+		<a href="" class="btn btn-primary btn-sm">success</a>
+		<a href="index.php/cart/add/<?= $product['id']?>" class="btn btn-success btn-sm">In den WArenkorb</a>
+	</div>
+</div>
