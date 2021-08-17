24d50d75335b9c39b5f28d62b89f10ea5b4e5687
L00tz1ffer <unknown@host.docker.internal>
2021-08-18 00:01:33 +0200

    OK, nicht erschrecken, ich fange von vorne an...

--- shop/index.php
+++ shop/index.php
@@ -1,8 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** Style Elements **/
+define ("BOOTSTRAP_DIR", "assets/bootstrap/");
+define ("CUSTOM_STYLESHEET", "assets/css/styles.css");
+
+/**Templates**/
+define ("TEMPLATE_DIR", "templates/default/");
+
+
+
 
